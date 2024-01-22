07fa09f5db43c76f961a12e4dfab71d543f610ed
mnoorcahyo <mnoorcahyo@gmail.com>
2024-01-22 12:44:42 +0700

    first commit

--- tests/KeyboardAction.spec.js
+++ tests/KeyboardAction.spec.js
@@ -1,9 +1,10 @@
 const {test,expect} = require('@playwright/test')
 
 test('Keyboard Action', async({page})=>{
-    await page.goto('')
+    await page.goto('https://gotranscript.com/text-compare')
 
-
+    // await page.locator('name="text1"').fill("Welcome to automation")
+    await page.ty('name="text1"', 'welcome to automation')
 
 
 
