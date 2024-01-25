2519ce875113dc0a83d55c2a1d2a2895eb25c1be
mnoorcahyo <mnoorcahyo@gmail.com>
2024-01-25 14:51:21 +0700

    Keyboard Action

--- tests/KeyboardAction.spec.js
+++ tests/KeyboardAction.spec.js
@@ -4,10 +4,23 @@ test('Keyboard Action', async({page})=>{
     await page.goto('https://gotranscript.com/text-compare')
 
     // await page.locator('name="text1"').fill("Welcome to automation")
-    await page.ty('name="text1"', 'welcome to automation')
+    await page.type('[name="text1"]', 'welcome to automation')
+
+
+    // Cmnd+A 
+    await page.keyboard.press('Meta+A')
+
+    // Cmnd+C
+    await page.keyboard.press('Meta+C')
+
+    // TAB
+    await page.keyboard.down('Tab')
+    await page.keyboard.up('Tab')
+
+    // Cmnd+V
+    await page.keyboard.press('Meta+V')
 
 
 
-
-    await page.waitForTimeout(5000)
+    await page.waitForTimeout(3000)
 })
